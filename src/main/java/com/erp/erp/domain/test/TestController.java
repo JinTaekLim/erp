5c6d1a0f0d4c415ae5621366c54117d177ff4e18
JinTaekLim <jintaek019@gmail.com>
2024-10-16 05:48:59 +0900

    Feat : 공통 응답 포맷 및 GlobalExceptionHandler 테스트를 위한 TestController 추가

---
+++ src/main/java/com/erp/erp/domain/test/TestController.java
@@ -0,0 +1,30 @@
+package com.erp.erp.domain.test;
+
+import com.erp.erp.global.error.ApiResult;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/test")
+@RequiredArgsConstructor
+@Slf4j
+public class TestController {
+
+  @GetMapping("/{status}")
+  @Schema(name = "테스트")
+  public ApiResult<String> test(@PathVariable String status) {
+    if (status.equals("success")) return ApiResult.success("success");
+    return ApiResult.fail("코드","메세지","데이터");
+  }
+
+  @GetMapping("/exception/{name}")
+  public void exception(){
+    throw new RuntimeException("default");
+  }
+
+}
