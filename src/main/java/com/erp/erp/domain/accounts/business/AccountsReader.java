87ec18c59b4236f63311b1e0291d6d9354e3c5b8
JinTaekLim <jintaek019@gmail.com>
2024-10-19 19:50:51 +0900

    feat. AuthService getAccountsInfo 메소드 1L 의 Id 값을 가지고 있는 Accounts 반환

--- src/main/java/com/erp/erp/domain/accounts/business/AccountsReader.java
+++ src/main/java/com/erp/erp/domain/accounts/business/AccountsReader.java
@@ -1,6 +1,8 @@
 package com.erp.erp.domain.accounts.business;
 
+import com.erp.erp.domain.accounts.common.entity.Accounts;
 import com.erp.erp.domain.accounts.repository.AccountsRepository;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -10,4 +12,9 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class AccountsReader {
   private final AccountsRepository accountsRepository;
+
+
+  public Optional<Accounts> findOptionalById(Long id) {
+    return accountsRepository.findById(id);
+  }
 }
