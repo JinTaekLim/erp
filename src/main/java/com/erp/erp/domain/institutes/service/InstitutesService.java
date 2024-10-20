0cb691e67beae4939e1f4cb37fcae23ed149a0c8
JinTaekLim <jintaek019@gmail.com>
2024-10-21 00:37:52 +0900

    refactor. 매장 회원 검증 로직 InstitutesService -> InstitutesValidator 이동

--- src/main/java/com/erp/erp/domain/institutes/service/InstitutesService.java
+++ src/main/java/com/erp/erp/domain/institutes/service/InstitutesService.java
@@ -3,6 +3,7 @@ package com.erp.erp.domain.institutes.service;
 import com.erp.erp.domain.customers.business.CustomersReader;
 import com.erp.erp.domain.customers.common.entity.Customers;
 import com.erp.erp.domain.institutes.business.InstitutesUpdater;
+import com.erp.erp.domain.institutes.business.InstitutesValidator;
 import com.erp.erp.domain.institutes.common.dto.UpdateTotalSpotsDto;
 import com.erp.erp.domain.institutes.common.entity.Institutes;
 import com.erp.erp.domain.institutes.common.exception.InstituteNotFoundInCustomerException;
@@ -16,6 +17,7 @@ import org.springframework.stereotype.Service;
 public class InstitutesService {
 
   private final InstitutesUpdater institutesUpdater;
+  private final InstitutesValidator institutesValidator;
   private final CustomersReader customersReader;
 
   public UpdateTotalSpotsDto.Response updateTotalSpots(
@@ -35,8 +37,6 @@ public class InstitutesService {
 
   public Customers validateCustomerBelongsToInstitute(Institutes institutes , Long customersId) {
     Customers customers = customersReader.findById(customersId);
-    Institutes comparativeInstitutes = customers.getInstitutes();
-    if (institutes == comparativeInstitutes) {return customers;}
-    else {throw new InstituteNotFoundInCustomerException();}
+    return institutesValidator.validateCustomerBelongsToInstitute(institutes,customers);
   }
 }
