0c0f5a8400d48347db15b8fe9b79f5b388369bd4
limjintaek <jintaek019@gmail.com>
2024-12-03 19:58:13 +0900

    feat. LocalTime 직렬화 케이스 추가 (HH:mm)

--- src/test/java/com/erp/erp/global/gson/LocalTimeAdapter.java
+++ src/test/java/com/erp/erp/global/gson/LocalTimeAdapter.java
@@ -4,18 +4,25 @@ import com.google.gson.*;
 import java.lang.reflect.Type;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 public class LocalTimeAdapter implements JsonSerializer<LocalTime>, JsonDeserializer<LocalTime> {
 
-  private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+  private static final DateTimeFormatter formatterWithSeconds = DateTimeFormatter.ofPattern("HH:mm:ss");
+  private static final DateTimeFormatter formatterWithoutSeconds = DateTimeFormatter.ofPattern("HH:mm");
 
   @Override
   public JsonElement serialize(LocalTime src, Type typeOfSrc, JsonSerializationContext context) {
-    return new JsonPrimitive(src.format(formatter));
+    return new JsonPrimitive(src.format(formatterWithSeconds));
   }
 
   @Override
   public LocalTime deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
-    return LocalTime.parse(json.getAsString(), formatter);
+    String timeString = json.getAsString();
+    try {
+      return LocalTime.parse(timeString, formatterWithSeconds);
+    } catch (DateTimeParseException e) {
+      return LocalTime.parse(timeString, formatterWithoutSeconds);
+    }
   }
 }
