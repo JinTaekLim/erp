a941e959fbfc361dc9755d2706d2a7a790d61d72
limjintaek <jintaek019@gmail.com>
2024-12-30 21:25:37 +0900

    feat. Plan 누락 테스트 추가

--- src/main/java/com/erp/erp/domain/plan/common/dto/GetPlanDto.java
+++ src/main/java/com/erp/erp/domain/plan/common/dto/GetPlanDto.java
@@ -3,18 +3,14 @@ package com.erp.erp.domain.plan.common.dto;
 import com.erp.erp.domain.plan.common.entity.LicenseType;
 import com.erp.erp.domain.plan.common.entity.PlanType;
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 public class GetPlanDto {
 
   @Schema(name = "GetPlans_Response" , description = "이용권 조회 반환")
   @Builder
   @Getter
-  @NoArgsConstructor
-  @AllArgsConstructor
   public static class Response{
 
     @Schema(description = "이용권 ID")
@@ -27,7 +23,6 @@ public class GetPlanDto {
     private String name;
     @Schema(description = "이용권 가격")
     private int price;
-
   }
 
 }
