f93c6ef911b5a6800fdb31717170f3295f0f1e83
limjintaek <jintaek019@gmail.com>
2025-02-12 00:09:14 +0900

    fix. 임시 인증/인가 코드 제거로 인한 테스트 코드 오류 수정

--- src/test/java/com/erp/erp/domain/plan/PlanTest.java
+++ src/test/java/com/erp/erp/domain/plan/PlanTest.java
@@ -1,13 +1,22 @@
-package com.erp.erp.domain.plan.controller;
+package com.erp.erp.domain.plan;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+import com.erp.erp.domain.account.common.entity.Account;
+import com.erp.erp.domain.account.repository.AccountRepository;
+import com.erp.erp.domain.auth.business.TokenManager;
+import com.erp.erp.domain.auth.common.dto.TokenDto;
+import com.erp.erp.domain.institute.common.entity.Institute;
+import com.erp.erp.domain.institute.repository.InstituteRepository;
 import com.erp.erp.domain.plan.common.dto.GetPlanDto;
 import com.erp.erp.domain.plan.common.entity.LicenseType;
 import com.erp.erp.domain.plan.common.entity.Plan;
 import com.erp.erp.domain.plan.repository.PlanRepository;
 import com.erp.erp.global.response.ApiResult;
+import com.erp.erp.global.util.HttpEntityUtil;
+import com.erp.erp.global.util.generator.AccountGenerator;
+import com.erp.erp.global.util.generator.InstituteGenerator;
 import com.erp.erp.global.util.generator.PlanGenerator;
 import com.erp.erp.global.util.randomValue.RandomValue;
 import com.erp.erp.global.test.IntegrationTest;
@@ -19,10 +28,12 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-class PlanControllerTest extends IntegrationTest {
+class PlanTest extends IntegrationTest {
 
   private String BASE_URL;
 
@@ -35,16 +46,37 @@ class PlanControllerTest extends IntegrationTest {
   private TestRestTemplate restTemplate;
 
   @Autowired
+  private AccountRepository accountRepository;
+
+  @Autowired
+  private InstituteRepository instituteRepository;
+
+  @Autowired
   private PlanRepository planRepository;
+
+  @Autowired
+  private TokenManager tokenManager;
 
   private Plan createPlan() {
     return planRepository.save(PlanGenerator.get());
+  }
+
+  private Account createAccount(Institute institute) {
+    return accountRepository.save(AccountGenerator.get(institute));
+  }
+
+  private Institute createInstitutes() {
+    return instituteRepository.save(InstituteGenerator.get());
   }
 
   @Test
   @DisplayName("getPlans 성공")
   void getPlans() {
     // given
+    Institute institute = createInstitutes();
+    Account account = createAccount(institute);
+    TokenDto tokenDto = tokenManager.createToken(account);
+
     LicenseType licenseType = RandomValue.getRandomEnum(LicenseType.class);
     int planCount = RandomValue.getInt(0,5);
 
@@ -56,9 +88,13 @@ class PlanControllerTest extends IntegrationTest {
 
     String url = BASE_URL + "/getPlans/" + licenseType;
 
+    HttpEntity<Void> httpRequest = HttpEntityUtil.setToken(null, tokenDto.getAccessToken());
+
     // when
-    ResponseEntity<String> responseEntity = restTemplate.getForEntity(
+    ResponseEntity<String> responseEntity = restTemplate.exchange(
         url,
+        HttpMethod.GET,
+        httpRequest,
         String.class
     );
 
@@ -85,15 +121,22 @@ class PlanControllerTest extends IntegrationTest {
   @DisplayName("요청 잘못된 값 전달")
   void getPlans_fail() {
     // given
+    Institute institute = createInstitutes();
+    Account account = createAccount(institute);
+    TokenDto tokenDto = tokenManager.createToken(account);
+
     int randomInt = RandomValue.getInt();
     String url = BASE_URL + "/getPlans/" + randomInt;
 
+    HttpEntity<Void> httpRequest = HttpEntityUtil.setToken(null, tokenDto.getAccessToken());
+
     // when
-    ResponseEntity<String> responseEntity = restTemplate.getForEntity(
+    ResponseEntity<String> responseEntity = restTemplate.exchange(
         url,
+        HttpMethod.GET,
+        httpRequest,
         String.class
     );
-
 
     ApiResult<List<GetPlanDto.Response>> apiResponse = gson.fromJson(
         responseEntity.getBody(),
