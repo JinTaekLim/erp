433f001e2c8a711ecf18b72249128ef87f84f169
limjintaek <jintaek019@gmail.com>
2025-02-27 17:29:20 +0900

    feat. RabbitMQ 초기 설정

---
+++ src/main/java/com/erp/erp/global/config/rabbitMq/RabbitMqProperties.java
@@ -0,0 +1,27 @@
+package com.erp.erp.global.config.rabbitMq;
+
+import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Getter
+@Setter
+@Configuration
+@ConfigurationProperties(prefix = "rabbitmq")
+public class RabbitMqProperties {
+  private List<QueueConfig> queues;
+
+  @Getter
+  @Setter
+  public static class QueueConfig {
+    private String name;
+    private String exchange;
+    private String routingKey;
+  }
+
+  public String getCustomerQueueName() {
+    return queues.get(0).getName();
+  }
+}
