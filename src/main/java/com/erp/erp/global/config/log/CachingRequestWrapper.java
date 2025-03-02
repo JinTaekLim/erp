d59a12d2fba76a8ae83085c52f9baadd4543b92c
limjintaek <jintaek019@gmail.com>
2025-03-02 15:45:50 +0900

    feat. Multipart 로깅

--- src/main/java/com/erp/erp/global/config/log/CachingRequestWrapper.java
+++ src/main/java/com/erp/erp/global/config/log/CachingRequestWrapper.java
@@ -1,5 +1,7 @@
 package com.erp.erp.global.config.log;
 
+import static org.springframework.web.multipart.support.MultipartResolutionDelegate.isMultipartRequest;
+
 import jakarta.servlet.ReadListener;
 import jakarta.servlet.ServletInputStream;
 import jakarta.servlet.http.HttpServletRequest;
@@ -7,8 +9,8 @@ import jakarta.servlet.http.HttpServletRequestWrapper;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Map;
 import org.springframework.util.StreamUtils;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 
 public class CachingRequestWrapper extends HttpServletRequestWrapper {
 
@@ -16,7 +18,10 @@ public class CachingRequestWrapper extends HttpServletRequestWrapper {
 
   public CachingRequestWrapper(HttpServletRequest request) throws IOException {
     super(request);
-    Map<String, String[]> parameterMap = request.getParameterMap();
+    if (isMultipartRequest(request)) {
+      StandardServletMultipartResolver multipartResolver = new StandardServletMultipartResolver();
+      multipartResolver.resolveMultipart(request);
+    }
     this.cachedContent = StreamUtils.copyToByteArray(request.getInputStream());
   }
 
