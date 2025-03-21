b4c1c9f68eb20dea7443b3bd23ae57529721a268
limjintaek <jintaek019@gmail.com>
2025-03-21 18:11:53 +0900

    conf. 스프링 세션 정책 변경

--- src/main/java/com/erp/erp/global/config/SecurityConfig.java
+++ src/main/java/com/erp/erp/global/config/SecurityConfig.java
@@ -11,10 +11,11 @@ import org.springframework.security.config.annotation.web.configurers.HeadersCon
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.context.SecurityContextHolderFilter;
+import org.springframework.session.web.http.CookieSerializer;
+import org.springframework.session.web.http.DefaultCookieSerializer;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
 import java.util.Arrays;
 import java.util.List;
 
@@ -55,5 +56,13 @@ public class SecurityConfig {
     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
     source.registerCorsConfiguration("/**", configuration);
     return source;
+  }
+
+  @Bean
+  public CookieSerializer cookieSerializer() {
+    DefaultCookieSerializer serializer = new DefaultCookieSerializer();
+    serializer.setSameSite("None");
+    serializer.setUseSecureCookie(false);
+    return serializer;
   }
 }
